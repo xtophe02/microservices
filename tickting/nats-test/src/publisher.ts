1f7e22986d2f7bb79fb663b97420eb6cbf3f5477
Christophe Moreira <christophe.moreira@outlook.com>
2020-06-04 15:55:02 +0100

    moved nats to common

--- tickting/nats-test/src/publisher.ts
+++ tickting/nats-test/src/publisher.ts
@@ -1,19 +1,32 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "../events/ticket-created-publisher";
 //kubectl port-forward POD 4222:4222
 console.clear();
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publiser connected to NATS");
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
 
-  stan.publish("ticket:created", data, () => {
-    console.log("Event Published");
-  });
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  // const data = JSON.stringify({
+  //   id: "123",
+  //   title: "concert",
+  //   price: 20,
+  // });
+
+  // stan.publish("ticket:created", data, () => {
+  //   console.log("Event Published");
+  // });
 });
