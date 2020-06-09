1be79f40e586a0d4b9c615b7fa64d12ba0b152db
Christophe Moreira <christophe.moreira@outlook.com>
2020-06-09 20:11:35 +0100

    added test /api/orders

---
+++ tickting/orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,45 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+import { Ticket } from "../../models/ticket";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@cmtickets/common";
+
+it("returns an error if ticket does not exist", async () => {
+  const ticketId = mongoose.Types.ObjectId();
+  request(app)
+    .post("/api/orders/")
+    .set("Cookie", global.signin())
+    .send({ ticketId })
+    .expect(404);
+});
+it("returns an error if ticket reserved", async () => {
+  const ticket = Ticket.build({ title: "test", price: 10 });
+  await ticket.save();
+  const order = Order.build({
+    userId: "1",
+    ticket,
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+
+  await order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+it("returns a ticket", async () => {
+  const ticket = Ticket.build({ title: "test", price: 10 });
+  await ticket.save();
+
+  const res = await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  // console.log(res.body);
+  expect(res.body.ticket.title).toEqual("test");
+});
--- tickting/orders/src/routes/new.ts
+++ tickting/orders/src/routes/new.ts
@@ -1,20 +1,60 @@
 import express, { Request, Response } from "express";
-import { requireAuth } from "@cmtickets/common";
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  BadRequestError,
+  OrderStatus,
+} from "@cmtickets/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders", requireAuth, async (req: Request, res: Response) => {
-  // const { title, price } = req.body;
-  // const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
-  // //THE SAVE AND PUBLIHSHER SHOULD BE DONE IN MONGODB TRANSACTION
-  // await ticket.save();
-  // await new TicketCreatedPublisher(natsWrapper.client).publish({
-  //   id: ticket.id,
-  //   title: ticket.title,
-  //   price: ticket.price,
-  //   userId: ticket.userId,
-  // });
-  // res.status(201).send(ticket);
-});
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    //Find the ticket the user is trying to order in the database
+    const { ticketId } = req.body;
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+    //Make sure that this ticket is not reserved
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) {
+      throw new BadRequestError("Ticket already reserved");
+    }
+    //CAlculate an expiration date for this order
+    const expiration = new Date();
+
+    //
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+    //Build the order and save to database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+    //Publise order:created
+    res.status(201).send(order);
+  }
+);
 
 export { router as createOrderRouter };
